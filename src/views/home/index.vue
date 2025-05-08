f19b19e0d9156843f853247c790bff8b4675d5db
JasmineGe <13530440233@163.com>
2025-05-08 17:19:09 +0800

    首页完成

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,13 +1,53 @@
 <template>
-  <div>
-    <h1>Home</h1>
+  <el-card>
+    <div class="box">
+      <img :src="userStore.avatar" class="avatar">
+      <div class="bottom">
+        <h3 class="title">{{ getTime() + '好' + userStore.username }}</h3>
+        <p class="subtitle">{{ setting.title }}</p>
+      </div>
+    </div>
+  </el-card>
+  <div class="bottoms">
+    <svg-icon name="welcome" width="500px" height="500px"></svg-icon>
   </div>
 </template>
 
 <script setup lang='ts'>
+import useUserStore from '@/store/modules/user';
+import { getTime } from '@/utils/time';
+import setting from '@/setting';
+
+let userStore = useUserStore()
 
 </script>
 
-<style scoped>
+<style scoped lang="scss">
 
+.box {
+  display: flex;
+  .avatar {
+    width: 100px;
+    height: 100px;
+    border-radius: 50%;
+  }
+  .bottom {
+    margin-left: 20px;
+    .title {
+      font-size: 30px;
+      font-weight: bold;
+      margin-bottom: 30px;
+    }
+    .subtitle {
+      font-style: italic;
+      color: skyblue;
+    }
+  }
+}
+.bottoms {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-top: 10px;
+}
 </style>
